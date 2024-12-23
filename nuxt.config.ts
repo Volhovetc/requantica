862158bd7703c9dc782090db2c182dfc21f0aab7
Mikhail <kocmoc777-88@mail.ru>
2024-12-23 20:02:21 +0500

    feat(create-project-requantica_0001): предпроцессор sass работает стабильно

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // css: ['~/assets/css/main.css'],
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
-  modules: [
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@nuxt/ui',
-  ]
+	// css: ['~/assets/css/main.css'],
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: false },
+	modules: ['@nuxt/fonts', '@nuxt/image', '@nuxt/icon', '@nuxt/ui'],
+	css: ['~/assets/styles/global.sass'],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				sass: {
+					additionalData: '@import "~/assets/styles/_mixins.sass"',
+				},
+			},
+		},
+		/*esbuild: {
+			drop: process.env.NODE_ENV === 'development' ? ['console'] : [],
+		},*/
+	},
 })
